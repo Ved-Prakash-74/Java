a72bd83036c63100eb1182b0d68316f356e8a509
Ved Prakash <96818066+Ved-Prakash-74@users.noreply.github.com>
2024-11-17 18:46:03 +0530

    Add files via upload

---
+++ PATTERN PROGRAMS/Mirror_Image_reverse_triangle_pattern_21.java
@@ -0,0 +1,43 @@
+// 1 2 3 4 5 6 
+//  2 3 4 5 6 
+//   3 4 5 6 
+//    4 5 6 
+//     5 6 
+//      6 
+//     5 6 
+//    4 5 6 
+//   3 4 5 6 
+//  2 3 4 5 6 
+// 1 2 3 4 5 6 
+
+public class Mirror_Image_reverse_triangle_pattern_21 
+{
+    public static void main(String [] args)
+    {
+        int n = 6;
+        for(int i = 1 ; i<= n ; i++)
+        {
+            for(int j = 1 ; j <= i ; j++)
+            {
+                System.out.print("  ");
+            }
+            for(int j = i ; j <= n ; j++)
+            {
+                System.out.print("  " + j + " ");
+            }
+            System.out.println();
+        }
+        for(int i = n ; i > 1 ; i--)
+        {
+            for(int j = 1 ; j <= i ; j++)
+            {
+                System.out.print("  ");
+            }
+            for(int j = i - 1 ; j <= n ; j++)
+            {
+                System.out.print("" + j + "  ");
+            }
+            System.out.println();
+        }
+    }
+}
